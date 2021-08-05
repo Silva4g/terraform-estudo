b42bc26d21dfb2267d73cacb4c97904a03849e4a
Kauan Silva <silva4g@pop-os.localdomain>
2021-08-05 17:00:02 -0300

    :zap: running aws

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
+  count         = 2
   ami           = "ami-0747bdcabd34c712a"
   instance_type = "t2.micro"
-  key_name      = "terraform-aws"
+  key_name      = "terraform"
+  tags = {
+    Name = "dev${count.index}"
+  }
 }
